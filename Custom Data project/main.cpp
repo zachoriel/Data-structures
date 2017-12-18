e5d90b1f0ff3f88336482e2038f6d3eb0706d699
Zach Oriel <s179065@aieeduau.onmicrosoft.com>
2017-12-18 09:31:33 -0800

    Proj

--- Custom Data project/main.cpp
+++ Custom Data project/main.cpp
@@ -1,150 +1,129 @@
-#include <iostream>
-#include <string>
-#include "test.h"
+#include<iostream>
 
 using namespace std;
 
-class Person
+struct node
 {
-public:
-	string name;
-	int age;
-	string status;
+	int data;
+	node *next;
 };
 
-int add(int a, int b)
+class list
 {
-	return a + b;
-}
-
-float add(float a, float b)
-{
-	return a + b;
-}
-
-template<typename T>
-T sub(T a, T b)
-{
-	return a - b;
-}
-
-template<typename T>
-void print(T value)
-{
-	std::cout << value;
-}
-
-template<>
-void print(Person value)
-{
-	cout << value.name << endl;
-	cout << value.age << endl;
-	cout << value.status << endl;
-}
-
-template<typename T>
-T minimum(T a, T b)
-{
-	if (a < b)
+public:
+	list()
 	{
-		return a;
+		head = NULL;
+		tail = NULL;
 	}
 
-	return b;
-}
+	void createNewNode(int value)
+	{
+		node *temp = new node;
+		temp->data = value;
+		temp->next = NULL;
 
-template <typename T>
-T maximum(T a, T b)
-{
-	if (a > b)
-	{
-		return a;
+		if (head == NULL)
+		{
+			head = temp;
+			tail = temp;
+			temp = NULL;
+		}
+		else
+		{
+			tail->next = temp;
+			tail = temp;
+		}
 	}
 
-	return b;
-}
+	void show()
+	{
+		node *temp = new node;
+		temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << "\t";
+			temp = temp->next;
+		}
+	}
 
-template<typename T>
-T clamped(T value, T lower, T upper)
-{
-	if (value < lower) 
+	void insertFront(int value)
 	{
-		return lower;
+		node *temp = new node;
+		temp->data = value;
+		temp->next = head;
+		head = temp;
 	}
-	else if (value > upper)
+
+	void insertBack(int value)
 	{
-		return upper;
+		node *temp = new node;
+		temp->data = value;
+		temp->next = tail;
+		tail = temp;
 	}
-	else
+
+	void deleteFront()
 	{
-		return value;
+		node *temp = new node;
+		temp = head;
+		head = head->next;
+		delete temp;
 	}
-}
+
+	void deleteBack()
+	{
+		node *current = new node;
+		node *previous = new node;
+		current = head;
+		while (current->next != NULL)
+		{
+			previous = current;
+			current = current->next;
+		}
+		tail = previous;
+		previous->next = NULL;
+		delete current;
+	}
+
+private:
+	node *head, *tail;
+};
+
 
 int main()
 {
-	Person userOne;
-	userOne.name = "Person 1: Doug";
-	userOne.age = 24;
-	userOne.status = "Status: in the pit of misery, dilly dilly";
+	list obj;
+	obj.createNewNode(25);
+	obj.createNewNode(50);
+	obj.createNewNode(90);
+	obj.createNewNode(40);
 
-	Person userTwo;
-	userTwo.name = "Person 2: Greg";
-	userTwo.age = 21;
-	userTwo.status = "Status: getting bud light";
-
-	Person userThree;
-	userThree.name = "Person 3: General Tullius";
-	userThree.age = 58;
-	userThree.status = "Status: dead! #skyrimbelongstothenords #forulfric";
-
-	Person userFour;
-	userFour.name = "Person 4: Ramsay Bolton";
-	userFour.age = 22;
-	userFour.status = "Status: eaten by his own dogs";
-
-	Person userFive;
-	userFive.name = "Person 5: Ajit Pai";
-	userFive.age = 44;
-	userFive.status = "Status: killing the internet";
-
-	assert<true>(2 == 2);
-	assertEqual(6, 6);
-	assertNotEqual(12, 52);
-
-	int lesser = minimum(1, 13);
-	print(lesser);
+	cout << "Showing All Nodes:" << endl;
+	obj.show();
 	cout << endl;
-
-	int bigger = maximum(2, 3);
-	print(bigger);
 	cout << endl;
-
-	int clamp = clamped(24, 40, 67); 
-	print(clamp);
+	cout << "Adding Node At The Back:" << endl;
+	obj.createNewNode(55);
+	obj.show();
 	cout << endl;
-									
-	float v1 = add(4.1f, 7.9f);
-	print(v1);
 	cout << endl;
-
-	float s1 = sub(1.5f, 2.1f);
-	int s2 = sub<int>(0, 2.0f);
-	print(s2 - s1);
+	cout << "Adding Node At The Front:" << endl;
+	obj.insertFront(10);
+	obj.show();
 	cout << endl;
-
 	cout << endl;
-
-	print(userOne);
+	cout << "Deleting Node At The Front:" << endl;
+	obj.deleteFront();
+	obj.show();
 	cout << endl;
-	print(userTwo);
 	cout << endl;
-	print(userThree);
-	cout << endl;
-	print(userFour);
-	cout << endl;
-	print(userFive);
+	cout << "Deleting Node At The Back:" << endl;
+	obj.deleteBack();
+	obj.show();
+	cout << "\n__________________________________________________\n";
 
 	system("pause");
+
 	return 0;
 }
